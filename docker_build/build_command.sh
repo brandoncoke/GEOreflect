23057deeafc27c7c8225b007630099a83d720883
Brandon <65897011+brandoncoke@users.noreply.github.com>
2025-03-24 13:37:45 +0000

    Update build_command.sh

--- docker_build/build_command.sh
+++ docker_build/build_command.sh
@@ -6,5 +6,5 @@ sudo docker create georeflect_shiny
 sudo docker container ls -a
 sudo docker export --output="georeflect_shiny.tar.gz" georeflect_shiny
 sudo docker container run --name georeflect_shiny --interactive --tty 
-docker run -ti -v .:/home georeflect_shiny R #run image and use current directory as the home for the image
+#docker run -ti -v .:/home georeflect_shiny R #run image and use current directory as the home for the image
 #sudo docker kill 
